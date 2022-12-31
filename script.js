c75014e9f6233cbd7e4c53d0c3608b79c4cd7a19
Petrelis-Efstratios <petrelis-stratos@outlook.com.gr>
2022-12-31 12:55:01 +0200

    Made the #main have a static width and  height

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let range = document.querySelector("input");
 let main = document.querySelector("#main");
 let p = document.querySelector("p");
+let size = main.clientWidth / range.value;
 for(let i = 0; i < range.value ** 2; i++) {
 const div = document.createElement("div");
 main.appendChild(div);
@@ -8,25 +9,28 @@ main.appendChild(div);
 let divs = document.querySelectorAll("#main > div");
 p.textContent = `${range.value} X ${range.value}`;
 divs.forEach((div) => {
+    div.setAttribute("style", `width: ${size}px; height: ${size}px;`);
     div.addEventListener("mouseover", () => {
     div.style.background = "blue"
     })
 });
 range.addEventListener("input", function() {
-        while (main.lastElementChild) {
-            main.removeChild(main.lastElementChild);
+    p.textContent = `${range.value} X ${range.value}`;
+    size = main.clientWidth / range.value;
+    while (main.lastElementChild) {
+        main.removeChild(main.lastElementChild);
+    }
+    for(let i = 0; i < range.value ** 2; i++) {
+        const div = document.createElement("div");
+        main.appendChild(div);
         }
-        for(let i = 0; i < range.value ** 2; i++) {
-            const div = document.createElement("div");
-            main.appendChild(div);
-            }
-            divs = document.querySelectorAll("#main > div");
-            divs.forEach((div) => {
-                div.addEventListener("mouseover", () => {
-                div.style.background = "blue"
-                })
+        divs = document.querySelectorAll("#main > div");
+        divs.forEach((div) => {
+            div.setAttribute("style", `width: ${size}px; height: ${size}px;`);
+            div.addEventListener("mouseover", () => {
+            div.style.background = "blue"
+            })
             });
-            main.setAttribute("style", `grid-template-columns: repeat(${range.value}, 0fr)`);
-            p.textContent = `${range.value} X ${range.value}`;
-}
+        main.setAttribute("style", `grid-template-columns: repeat(${range.value}, 0fr)`);
+    }
 );
